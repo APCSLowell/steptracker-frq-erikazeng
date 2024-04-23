c5fc923000a927ee1113c577e7af3f4c1c43721b
erikazeng <143123644+erikazeng@users.noreply.github.com>
2024-04-22 21:57:14 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -4,7 +4,7 @@ public class StepTracker{
  private int totalSteps;
  private int numDays;
  private int numActiveDays;
-public StepTracker(int threshold){\
+public StepTracker(int threshold){
  minSteps = threshold;
  totalSteps = 0;
  numDays = 0;
